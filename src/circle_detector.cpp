69a67d4378f3fc81f215f9fe0daf664806a8ed9f
Sergi <sergi@sergi-Satellite-A660>
2016-11-05 16:26:07 +0100

    Done, try it

--- src/circle_detector.cpp
+++ src/circle_detector.cpp
@@ -1,15 +1,14 @@
-
 //OpenCV 
-#include "cv.h"
-#include "highgui.h"
-#include "opencv2/core/core.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
 
-//std
+//Std
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
-//Declaración de las constantes que intervienen en el reconocimiento
+//Decl. Constants
 const int GAUSSIAN_BLUR_SIZE = 7;
 const double GAUSSIAN_BLUR_SIGMA = 2; 
 const double CANNY_EDGE_TH = 150;
@@ -21,28 +20,21 @@ const int MAX_RADIUS = 100;
 
 int main(int argc, char *argv[]) 
 {
-    cv::VideoCapture camera; //OpenCV video capture object
-    cv::Mat image; //OpenCV image object
-	int cam_id; //camera id . Associated to device number in /dev/videoX
+    cv::VideoCapture camera; 	//OpenCV video capture object
+    cv::Mat image; 		//OpenCV image object
+	int cam_id; 		//Camera id . Associated to device number in /dev/videoX
     cv::Mat gray_image;
-    cv::vector<cv::Vec3f> circles;
+    std::vector<cv::Vec3f> circles;
     cv::Point center;
     int radius;
     
-
-// 	cv::Ptr<cv::ORB> orb_detector = new cv::ORB(MIN_NUM_FEATURES); //ORB point feature detector
-//     cv::vector<cv::KeyPoint> point_set; //set of point features
-//     cv::Ptr<cv::DescriptorExtractor> orb_descriptor; //ORB descriptor
-//     orb_descriptor = cv::DescriptorExtractor::create("ORB"); //init the descriptor
-//     cv::Mat descriptor_set; //set of descriptors, for each feature there is an associated descriptor 
-	
-	//check user args
+	//Check user args
 	switch(argc)
 	{
-		case 1: //no argument provided, so try /dev/video0
+		case 1: //No argument provided, so try /dev/video0
 			cam_id = 0;  
 			break; 
-		case 2: //an argument is provided. Get it and set cam_id
+		case 2: //An argument is provided. Get it and set cam_id
 			cam_id = atoi(argv[1]);
 			break; 
 		default: 
@@ -51,10 +43,10 @@ int main(int argc, char *argv[])
 			break; 
 	}
 	
-	//advertising to the user 
-	std::cout << "Opening circle detector program" << cam_id << std::endl;
+	//Advertising to the user 
+	std::cout << "Opening video device " << cam_id << std::endl;
 
-    //open the video stream and make sure it's opened
+    //Open the video stream and make sure it's opened
     if( !camera.open(cam_id) ) 
 	{
         std::cout << "Error opening the camera. May be invalid device id. EXIT program." << std::endl;
@@ -64,19 +56,19 @@ int main(int argc, char *argv[])
     //Process loop. Capture and point feature extraction. User can quit pressing a key
     while(1)
 	{
-		//Read image and check it. Blocking call up to a new image arrives from camera.
+	//Read image and check it. Blocking call up to a new image arrives from camera.
         if(!camera.read(image)) 
 		{
             std::cout << "No image" << std::endl;
             cv::waitKey();
         }
         		
-   //**************** Find circles in the image ****************************
+    	//**************** Find circles in the image ****************************
         
-        //clear previous circles
+        //Clear previous circles to avoid overlays
         circles.clear();
 
-        // If input image is RGB, convert it to gray 
+        //If input image is RGB, convert it to gray 
         cv::cvtColor(image, gray_image, CV_BGR2GRAY);
 
         //Reduce the noise so we avoid false circle detection
@@ -85,24 +77,24 @@ int main(int argc, char *argv[])
         //Apply the Hough Transform to find the circles
         cv::HoughCircles( gray_image, circles, CV_HOUGH_GRADIENT, HOUGH_ACCUM_RESOLUTION, MIN_CIRCLE_DIST, CANNY_EDGE_TH, HOUGH_ACCUM_TH, MIN_RADIUS, MAX_RADIUS );
         
-        //draw circles on the image      
+        //Draw circles on the image      
         for(unsigned int ii = 0; ii < circles.size(); ii++ )
         {
             if ( circles[ii][0] != -1 )
             {
                     center = cv::Point(cvRound(circles[ii][0]), cvRound(circles[ii][1]));
                     radius = cvRound(circles[ii][2]);
-                    cv::circle(image, center, 5, cv::Scalar(0,255,255), -1, 8, 0 );// circle center in yellow
-                    cv::circle(image, center, radius, cv::Scalar(0,0,255), 3, 8, 0 );// circle perimeter in red
+                    cv::circle(image, center, 5, cv::Scalar(255,0,0), -1, 8, 0 );// Circle center in blue
+                    cv::circle(image, center, radius, cv::Scalar(0,0,255), 2, 3, 0 );// Circle perimeter in red
             }
-        }  
+        }      
+        
     //********************************************************************
     
-        //show image
-        cv::namedWindow( "Hough Circle Transform Demo", CV_WINDOW_AUTOSIZE );
-        cv::imshow("Hough Circle Transform Demo", image);
+        //Show image
+        cv::imshow("Find circles Show", image);
 
-		//Waits 1 millisecond to check if a key has been pressed. If so, breaks the loop. Otherwise continues.
+	//Waits 1 millisecond to check if a key has been pressed. If so, breaks the loop. Otherwise continues.
         if(cv::waitKey(1) >= 0) break;
     }   
 }
